9a17bc7a120fb446379453ba16b61a9a065892fb
André Duarte <aduarte@ar2tech.com>
2021-01-24 21:07:50 -0300

    feat: add post template

--- front/src/Paginas/Post.js
+++ front/src/Paginas/Post.js
@@ -1,6 +1,8 @@
+import { useState } from 'react'
 import styled from 'styled-components'
 import { useParams } from 'react-router-dom'
-import PostComponent from '../Componentes/PostComponent'
+import ReadPost from '../Componentes/ReadPost'
+import EditPost from '../Componentes/EditPost'
 
 const post = {
   id: 1,
@@ -15,17 +17,53 @@ const post = {
   autor: "Nome do autor",
 }
 
+const comentarios = [
+  {
+    conteudo: "conteudo de um comentario mega importante pro blog",
+    usuario: {
+      nome: "Nome do usuario"
+    },
+  },
+  {
+    conteudo: "conteudo de um comentario mega importante pro blog",
+    usuario: {
+      nome: "Nome do usuario"
+    },
+  },
+  {
+    conteudo: "conteudo de um comentario mega importante pro blog",
+    usuario: {
+      nome: "Nome do usuario"
+    },
+  },
+  {
+    conteudo: "conteudo de um comentario mega importante pro blog",
+    usuario: {
+      nome: "Nome do usuario"
+    },
+  },
+  {
+    conteudo: "conteudo de um comentario mega importante pro blog",
+    usuario: {
+      nome: "Nome do usuario"
+    },
+  },
+]
+
 const Container = styled.div`
-  margin: 20px;
   margin: 0 auto;
   max-width: 1024px;
 `
 
 const Post = () => {
   const { id } = useParams()
+  const [editMode, setEditMode] = useState(false)
   return (
     <Container>
-      <PostComponent {...post} />
+      {(editMode ?
+        <EditPost post={post} /> :
+        <ReadPost post={post} comentarios={comentarios} />
+      )}
     </Container>
   )
 }
