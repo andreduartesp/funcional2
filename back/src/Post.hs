ebd91f72203669535040d4a657d1c8493f6dcb29
Murilo Azevedo <muriloazevedo338@gmail.com>
2021-01-25 11:56:09 -0300

    fix post insert

--- back/src/Post.hs
+++ back/src/Post.hs
@@ -8,26 +8,29 @@ module Post where
 
 import Foundation
 import Yesod
+import Yesod.Core
+import Data.Text
+import Control.Monad.IO.Class (liftIO)
 
 instance ToJSON Post where
-    toJSON (Post titulo conteudo autorId) = object ["titulo" .= titulo, "conteudo" .= conteudo, "autorId" .= autorId]
+    toJSON (Post titulo conteudo editorId) = object ["titulo" .= titulo, "conteudo" .= conteudo, "editorId" .= editorId]
 
 instance FromJSON Post where
     parseJSON (Object v) = do
         titulo <- v .: "titulo"
         conteudo <- v .: "conteudo"
-        autorId <- v .: "autorId"
-        return (Post titulo conteudo autorId)
+        editorId <- v .: "editorId"
+        return (Post titulo conteudo editorId)
 
 getPostR :: PostId -> Handler Value
 getPostR postId = do
     post <- runDB $ get404 postId
     returnJson post
 
-postPostR :: PostId -> Handler Value
-postPostR postId = do
+postPostsR :: Handler Value
+postPostsR = do
     post <- requireInsecureJsonBody :: Handler Post
-    postId <- insert post
+    postId <- runDB $ insert post
     return $ object ["id" .= postId]
 
 putPostR :: PostId -> Handler Value
@@ -38,5 +41,6 @@ putPostR postId = do
 
 getPostsR :: Handler Value
 getPostsR = do
-    -- selectList [ComentarioPostId ==. postId] []
-    return $ object ["teste" .= True]
+    posts <- runDB $ selectList [] [Asc PostTitulo]
+    liftIO $ print posts
+    return $ object ["results" .= True]
