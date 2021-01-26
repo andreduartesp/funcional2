2b1a0507b6e7860c22754d7ffb350660c3403e3b
Murilo Azevedo <muriloazevedo338@gmail.com>
2021-01-26 13:44:51 -0300

    fetch comentarios by postId

--- back/src/Comentarios.hs
+++ back/src/Comentarios.hs
@@ -8,21 +8,48 @@ module Comentarios where
 
 import Foundation
 import Yesod
+import qualified Database.Esqueleto      as E
+import           Database.Esqueleto      ((^.))
 
+data ComentarioData = ComentarioData(Entity Comentario, Entity Usuario)
+
+instance ToJSON ComentarioData where
+    toJSON (ComentarioData (comentarioEntity, usuarioEntity)) =
+        let
+            comentario = entityVal comentarioEntity
+            comentarioId = entityKey comentarioEntity
+            usuario = entityVal usuarioEntity
+            usuarioId = entityKey usuarioEntity
+        in
+        object
+        [ "id" .= comentarioId
+        , "conteudo" .= comentarioConteudo comentario
+        , "usuario" .= object
+            [ 
+              "nome" .= usuarioNome usuario
+            , "usuario" .= usuarioUsuario usuario
+            ]
+        ]
 instance ToJSON Comentario where
-    toJSON (Comentario conteudo postId usuarioId) = object ["conteudo" .= conteudo, "postId" .= postId, "usuarioId" .= usuarioId]
+    toJSON (Comentario conteudo comentarioId usuarioId) = object ["conteudo" .= conteudo, "comentarioId" .= comentarioId, "usuarioId" .= usuarioId]
 
 instance FromJSON Comentario where
     parseJSON (Object v) = do
         conteudo <- v .: "conteudo"
-        postId <- v .: "postId"
+        comentarioId <- v .: "comentarioId"
         usuarioId <- v .: "usuarioId"
-        return (Comentario conteudo postId usuarioId)
+        return (Comentario conteudo comentarioId usuarioId)
 
 getComentariosR :: PostId -> Handler Value
 getComentariosR postId = do
-    runDB $ selectList [ComentarioPostId ==. postId] []
-    return $ object ["teste" .= True]
+    allComentarios <- runDB
+         $ E.select
+         $ E.from $ \(comentario `E.InnerJoin` usuario) -> do
+              E.on $ comentario ^. ComentarioUsuarioId E.==. usuario ^. UsuarioId
+              E.orderBy [E.desc (comentario ^. ComentarioId)]
+              return (comentario, usuario)
+    let allComentarios' = ComentarioData <$>  allComentarios
+    return $ object ["results" .= allComentarios']
 
 postComentarioR ::  Handler Value
 postComentarioR = do
