9dc304f414a8322dd26e81b6f1ac130ce24cf5c2
André Duarte <aduarte@ar2tech.com>
2021-01-26 20:17:37 -0300

    feat: add editor login

--- back/app/Main.hs
+++ back/app/Main.hs
@@ -1,21 +1,18 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE QuasiQuotes #-}
 
 import Application () -- for YesodDispatch instance
 import Foundation
 import Yesod.Core
 import Yesod.Static
-import Data.Text (Text, pack)
+
 import Database.Persist.Postgresql
 import Control.Monad.Logger (runStdoutLoggingT)
 import Control.Monad.IO.Class (liftIO)
-import System.Environment
 
-connStr = read <$> getEnv "DATABASE_URL"
+connStr = "dbname=funcional host=localhost user=myuser password=mypass port=5432"
 
 main::IO()
 main = runStdoutLoggingT $ withPostgresqlPool connStr 10 $ \pool -> liftIO $ do
     flip runSqlPersistMPool pool $ do
         runMigration migrateAll
-    port <- read <$> getEnv "PORT"
-    warp port (App pool)
+    warp 8080 (App pool)
--- front/src/Paginas/LoginEditor.js
+++ front/src/Paginas/LoginEditor.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import styled from 'styled-components'
-import LoginForm from '../Componentes/LoginForm'
+import LoginEditorForm from '../Componentes/LoginEditorForm'
 
 const Container = styled.div`
   margin: 0 auto;
@@ -37,7 +37,7 @@ const LoginEditor = () => {
       </HeaderContainer>
       <FakeContainer></FakeContainer>
       <Container>
-        <LoginForm />
+        <LoginEditorForm />
       </Container>
     </>
   )
