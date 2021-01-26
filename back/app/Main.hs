d78ec5dc6f6943b2cc34ce37b68601c2f7a12f84
Murilo Azevedo <muriloazevedo338@gmail.com>
2021-01-26 20:24:49 -0300

    add Procile and change credentials

--- back/app/Main.hs
+++ back/app/Main.hs
@@ -11,11 +11,10 @@ import Control.Monad.Logger (runStdoutLoggingT)
 import Control.Monad.IO.Class (liftIO)
 import System.Environment
 
-connStr = read <$> getEnv "DATABASE_URL"
+connStr = "postgres://rwheorhgzwgmsv:2b79672ba18e6b8e9356a6071e14a96fb8fbabc48a8e7fe04ce1d8e11de1bef9@ec2-34-237-89-96.compute-1.amazonaws.com:5432/d88nt9pi0s21kq"
 
 main::IO()
 main = runStdoutLoggingT $ withPostgresqlPool connStr 10 $ \pool -> liftIO $ do
     flip runSqlPersistMPool pool $ do
         runMigration migrateAll
-    port <- read <$> getEnv "PORT"
-    warp port (App pool)
+    warp 8080 (App pool)
