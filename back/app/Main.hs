b5d7181de701b6e62fb735e9f6d10cc07f6849d4
Murilo Azevedo <muriloazevedo338@gmail.com>
2021-01-26 21:56:52 -0300

    fix port from env

--- back/app/Main.hs
+++ back/app/Main.hs
@@ -8,6 +8,7 @@ import Yesod.Static
 import Database.Persist.Postgresql
 import Control.Monad.Logger (runStdoutLoggingT)
 import Control.Monad.IO.Class (liftIO)
+import System.Environment
 
 connStr = "postgres://rwheorhgzwgmsv:2b79672ba18e6b8e9356a6071e14a96fb8fbabc48a8e7fe04ce1d8e11de1bef9@ec2-34-237-89-96.compute-1.amazonaws.com:5432/d88nt9pi0s21kq"
 
@@ -15,4 +16,5 @@ main::IO()
 main = runStdoutLoggingT $ withPostgresqlPool connStr 10 $ \pool -> liftIO $ do
     flip runSqlPersistMPool pool $ do
         runMigration migrateAll
-    warp 8080 (App pool)
+    port <- read <$> getEnv "PORT"
+    warp port (App pool)
